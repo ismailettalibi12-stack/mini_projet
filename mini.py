fce32eab533cb89df6014fc2907082403c7bff5d
ettalibi ismail <ismailettalibi12@gmail.com>
2025-11-27 19:55:11 +0100

    v1

---
+++ mini.py
@@ -0,0 +1,62 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": 3,
+   "id": "6eac5e8a-9814-4b04-b224-cdcc4ccf8984",
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdin",
+     "output_type": "stream",
+     "text": [
+      "give me hours used : 3\n",
+      "give me power :  23\n"
+     ]
+    },
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "cost is  0.069\n"
+     ]
+    }
+   ],
+   "source": [
+    "a=int(input(\"give me hours used :\"))\n",
+    "p=int(input(\"give me power : \"))\n",
+    "c=p*a*0.001\n",
+    "print(\"cost is \",c)"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "2a98d869-5ba6-4f32-9374-e22aa03d7777",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3 (ipykernel)",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.12.4"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
